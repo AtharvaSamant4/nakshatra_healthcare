64fdd27330086508a2304250b2779a3943b31c75
AtharvaSamant4 <atharvasamant43@gmail.com>
2026-04-05 08:20:15 +0530

    remove root lockfile

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.nakshatra.healthcare',
+  appName: 'NakshatraHealthcare',
+  webDir: 'out'
+};
+
+export default config;
